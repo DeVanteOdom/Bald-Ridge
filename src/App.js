fc18b77b6e8912b76e2b5371f1d26c8c817f2431
CerealHero <72399578+CerealHero@users.noreply.github.com>
2021-05-24 14:24:02 -0400

    React router added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router';
 
 function App() {
   return (
+    <Switch>
+      <Route path='/Bald'>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +22,8 @@ function App() {
         </a>
       </header>
     </div>
+    </Route>
+    </Switch>
   );
 }
 
