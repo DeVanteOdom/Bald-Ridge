2ad556f83cae077c9a3ec6b181cf64cc4fad04e8
Jonny Deates <jonnydeates@gmail.com>
2021-06-02 17:09:22 -0500

    Nav CSS Changes

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -3,7 +3,9 @@ import "./Navigation.css"
 import logo from "../../assets/logo/logo.png"
 export const Navigation = ({paths, nav}) => 
 <nav className="nav">
-    <Link to="/"><img src={logo} title={"Goto Home Page"} alt={'Home'}/></Link>
+    <Link to="/">
+        {window.screen.width > 480 ? <img src={logo} title={"Goto Home Page"} alt={'Home'}/> : "Home"}
+    </Link>
     {paths.map((path,i)=> <Link to={'/'+path.to} className={nav===window.location ? 'navActive' : ''} title={`Goto ${path.name}`}>{path.name}</Link>)}
     <a href="https://baldridgelodge.networkforgood.com/projects/74020-bald-ridge-lodge-annual">Donate</a>
 </nav>
