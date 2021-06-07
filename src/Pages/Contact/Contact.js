2c482f6b3516925802fe80af972cc137f0e16c7e
Jonny Deates <jonnydeates@gmail.com>
2021-06-07 14:27:05 -0500

    Contact Page

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,9 +1,33 @@
 import React, {Component} from 'react';
 import "./Contact.css"
+import {ParallaxBanner} from "react-scroll-parallax";
+import backgroundImg from "../../assets/backgrounds/contact-background.jpg";
 class Contact extends Component {
     render() {
         return <div className="contact">
-            Contact
+            <ParallaxBanner className={'contact-parallaxing'}
+                style={{objectFit: "cover", height: "30vh"}}
+                layers={[{image: backgroundImg, amount: 0.5}]}>
+                <h1>Contact</h1>
+                <p>Have any questions or want to reach out?</p>
+            </ParallaxBanner>
+            <div className={'contact-description'}>
+                <p>Telephone: <span>770-887-1220</span></p>
+                <p>Fax: <span>770-887-4161</span></p>
+                <p>Mail Us: <span>505 Lakeland Plaza, #302 Cumming, GA 30040</span></p>
+            </div>
+            <div className={'contact-card'}>
+                <p>Write Us!</p>
+                <div>
+                    <input placeholder={'Name'}/>
+                    <input placeholder={'Email'}/>
+                </div>
+                <div className={'contact-email'}>
+                    <input placeholder={'Subject'}/>
+                    <textarea placeholder={'Content'}/>
+                </div>
+                <button>Submit</button>
+            </div>
         </div>
     }
 }
